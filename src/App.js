d70b6f9213e169b3f0460da61f8e0b55b9127817
Fatima Jangda <fjangda7@gmail.com>
2021-06-30 00:54:16 -0400

    simple blockchain interaction w web3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+import {useState, useEffect} from "react";
 
 function App() {
+  console.log(web3.version);
+  web3.eth.getAccounts().then(console.log);
+
+  const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const manager = await lottery.methods.manager().call(); 
+      const players = await lottery.methods.getPlayers().call(); 
+      const balance = await web3.eth.getBalance(lottery.options.address);
+
+      setManager(manager);
+      setPlayers(players);
+      setBalance(balance);
+    }
+    fetchData();
+
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts(); 
+
+    setMessage('Waiting on transaction success...');
+    await lottery.methods.enter().send({from: accounts[0], value: web3.utils.toWei(value, 'ether')});
+    setMessage('You have been entered!');
+
+  };
+
+  const onClick = async(event) => {
+    const accounts = await web3.eth.getAccounts(); 
+
+    setMessage('Waiting on transaction success...');
+    await lottery.methods.pickWinner().send({from: accounts[0]});
+    setMessage('A winner has been picked!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {manager}. 
+        There are currently {players.length} people entered, competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
+      <hr/> 
+      <form onSubmit={onSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input onChange = {event => setValue(event.target.value)}/>
+          <button>Enter</button>
+        </div>
+      </form>
+      <hr/>
+      <hr/>
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={onClick}>Pick a winner!</button>
+
+      <h1>{message}</h1>
     </div>
   );
 }
